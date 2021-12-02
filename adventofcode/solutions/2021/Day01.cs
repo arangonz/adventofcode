85f50e2e6b95163ca970d1a23b0b87bb9edbefba
AranGonz <francisco.arangonz@gmail.com>
2021-12-02 02:12:28 +0100

    structure and creation of day 1 files

---
+++ adventofcode/solutions/2021/Day01.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode.solutions._2021
+{
+    internal static class Day01
+    {
+        static void PrintSolutions()
+        {
+        }
+    }
+}
